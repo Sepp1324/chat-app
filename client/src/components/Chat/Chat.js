d374792a9d9749d82472326b737b421b50fe2647
Sebastian Stögerer <sebastian@localhost.localdomain>
2020-08-04 13:18:11 +0200

    Worked on css

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './chat.css'
+
 const Chat = () => {
     return <h1>Chat</h1>
 }
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './join.css'
+
+const Join = () => {
+    const [name, set_name] = useState('');
+    const [room, set_room] = useState('');
+
+    return <div className="join-outer-container">
+        <div className="join-inner-container">
+            <h1 className="heading">Join</h1>
+            <div><input type="text" className="join-input" onChange={(event) => set_name(event.target.value)} /></div>
+            <div><input type="text" className="join-input mt-20" onChange={(event) => set_room(event.target.value)} /></div>
+            <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                <button className="button mt-20" type="submit">Sign In</button>
+            </Link>
+        </div>
+    </div>
+}
+
+export default Join;
