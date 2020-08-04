62ab6139c16c34a07710f8d25d3ea59a479f9451
Sebastian Stögerer <sebastian@localhost.localdomain>
2020-08-04 12:59:53 +0200

    Connections

--- server/index.js
+++ server/index.js
@@ -3,9 +3,19 @@ const socket_io = require('socket.io');
 const http = require('http');
 
 const port = process.env.PORT || 5000;
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socket_io(server);
 
+io.on('connection', (socket) => {
+    console.log('Connection established!');
+
+    socket.on('disconnect', () => {
+        console.log('Connection terminated!');
+    });
+});
+
+app.use(router);
 server.listen(port, () => console.log(`Server has started on port ${port}`));
