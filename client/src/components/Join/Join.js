52e541afecbcb87246210d0ac3a0053fe922ea8a
Sebastian Stögerer <sebastian@localhost.localdomain>
2020-08-04 13:23:20 +0200

    Implemented input form

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import './join.css'
@@ -10,8 +10,8 @@ const Join = () => {
     return <div className="join-outer-container">
         <div className="join-inner-container">
             <h1 className="heading">Join</h1>
-            <div><input type="text" className="join-input" onChange={(event) => set_name(event.target.value)} /></div>
-            <div><input type="text" className="join-input mt-20" onChange={(event) => set_room(event.target.value)} /></div>
+            <div><input type="text" placeholder="Name" className="join-input" onChange={(event) => set_name(event.target.value)} /></div>
+            <div><input type="text" placeholder="Room" className="join-input mt-20" onChange={(event) => set_room(event.target.value)} /></div>
             <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                 <button className="button mt-20" type="submit">Sign In</button>
             </Link>
